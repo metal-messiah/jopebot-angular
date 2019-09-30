01dd33d48c698b806419700efa6025bf3a95bdb9
Jordan Porter <jordan@mtnra.com>
2019-09-30 09:13:01 -0700

    clean up

--- src/app/users/request/request.component.ts
+++ src/app/users/request/request.component.ts
@@ -15,6 +15,15 @@ import {
   Validators
 } from "@angular/forms";
 
+import {
+  debounce,
+  debounceTime,
+  delay,
+  finalize
+} from "rxjs/internal/operators";
+
+import * as Fuse from "fuse.js";
+
 @Component({
   selector: "app-request",
   templateUrl: "./request.component.html",
@@ -25,25 +34,35 @@ export class RequestComponent implements OnInit {
   fetching = { songs: false };
   songs: StreamerSong;
 
+  display: any[] = [];
+
+  selectIsOpen = false;
+
   dataFields: string[] = [];
+
   fieldsControl: FormControl;
+  textControl: FormControl;
+
+  fuse: Fuse<any>;
 
   constructor(
     private route: ActivatedRoute,
-    private streamerSongsService: StreamerSongsService,
-    private formBuilder: FormBuilder
+    private streamerSongsService: StreamerSongsService
   ) {}
 
   ngOnInit() {
     this.route.paramMap.subscribe(async params => {
       this.streamerIdParam = params.get("userid");
-      console.log(this.streamerIdParam);
-      this.updateSongs();
+      this.fetchSongs();
+    });
+
+    this.textControl = new FormControl("", [Validators.required]);
+    this.textControl.valueChanges.pipe(debounceTime(500)).subscribe(val => {
+      this.search(val);
     });
   }
 
-  async updateSongs() {
-    console.log("update songs");
+  async fetchSongs() {
     this.fetching.songs = true;
     this.streamerSongsService
       .getAll({ user_id: this.streamerIdParam })
@@ -51,16 +70,46 @@ export class RequestComponent implements OnInit {
         this.songs = resp.length ? resp[0] : null;
         this.fetching.songs = false;
 
-        console.log(this.songs);
-
         this.dataFields = Object.keys(this.songs.data[0]);
+        this.display = this.songs.data;
+        console.log(this.display.length);
 
         // by default show 6 fields
-        this.fieldsControl = new FormControl(this.dataFields.slice(0, 6), [
+        this.fieldsControl = new FormControl(this.dataFields.slice(0, 4), [
           Validators.required
         ]);
-        console.log(this.fieldsControl);
+
         this.fetching.songs = false;
       });
+  }
+
+  getRowWidth(): string {
+    return `${window.innerWidth / this.fieldsControl.value.length - 10}px`;
+  }
+
+  onBlur() {
+    this.selectIsOpen = false;
+  }
+
+  onFocus() {
+    this.selectIsOpen = true;
+  }
+
+  search(term: string) {
+    const options = {
+      shouldSort: true,
+      threshold: 0.6,
+      location: 0,
+      distance: 100,
+      maxPatternLength: 32,
+      minMatchCharLength: 1,
+      keys: this.dataFields
+    };
+    this.fuse = new Fuse(this.songs.data, options); // "list" is the item array
+    this.display = this.fuse.search(term);
+  }
+
+  onSongClick(song: any) {
+    console.log(`Click on Song ${song.songName}`);
   }
 }
